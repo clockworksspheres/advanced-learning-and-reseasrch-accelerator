bd39f490b90a3afb96f2e10cac42787f6e357212
Roy <roy.nielsen17@gmail.com>
2024-02-10 11:40:02 -0700

    adding RisDataModel

--- design/RISinfo.md
+++ design/RISinfo.md
@@ -6,12 +6,13 @@ Research Information Systems (RIS) format related information
 RIS has two tables of information, #TAG #TYPE #RIS_TAG #RIS_TYPE
 
 It's an older format so wikipedia should be just fine using, or at least referencing the wikipedia reference to start.
+
 # References
 
 * [Wikipedia info on RIS formats](https://en.wikipedia.org/wiki/RIS_%28file_format%29)
 
 # Additional formats
 
-[ENW Refer BibIX EndNote tag map](https://github.com/aurimasv/translators/wiki/ENW-Refer-BibIX-EndNote-tag-map)
+ * [ENW Refer BibIX EndNote tag map](https://github.com/aurimasv/translators/wiki/ENW-Refer-BibIX-EndNote-tag-map)
 
 
---
+++ src/RisDataModel.py
@@ -0,0 +1,76 @@
+
+
+
+class RisDataModel(){
+    """
+    """
+    __init__()
+    {
+        risData {
+
+            types = { “ABST” : “Abstract”,
+              “ANCIENT” : “Ancient Text”
+              “BLOG” : “Blog”,
+              “BOOK” : “Book”,
+              “CASE“ : “Legal case and case notes”,
+              “CHAP“ : “Book section/chapter”,
+              “COMP” : “Computer program”,
+              “CONF” : “Conference proceedings”,
+              “CPAPER” : “Conference paper”,
+                     :    “EBOOK” : “Electronic Book”,
+              “ECHAP” : “Electronic book section”,
+              “EDBOOK” : “Edited book”,
+              “EJOUR” : “Electronic article”,
+              “ELEC” : “Web page / electronic citation”,
+              “JFULL” : “Journal/periodical (full)”,
+              “JOUR” : “Journal/(article)”,
+              “MANSCPT” : “Manuscript”,
+              “PAMP” : “Pamphlet”,
+              “PAT” : “Patent”,
+              “PCOMM” : “Personal communication”,
+              “POD” : “Podcast”,
+              “RPRT” : “Report”,
+              “WEB” : “Web page”
+            }
+
+            tags { “A1” : "Primary author, synonym of AU”,
+              “A2” : “Secondary author/editor/translator”,
+              “A3” : “Tertiary author/editor/translator”,
+              “A4” : “Quaternary author/editor/translator”,
+              “A5” : “Quinary author/compiler”,
+              “A6” : “Website author”,
+              “AB” : “Abstract or synopsis. Notes also N2”,
+              “AD” : “(author/editor/inventor) address, eg postal address, email, phone, “fax. Institution.”,
+              “DOI” : “Digital Object Identifier”,
+              “EP” : “Pages”,
+              “ET” : “Edition”,
+              “J1” : “Notes”,“J2” : “Alternative title”,
+              “JA” : “Standard abbreviation for Journal/periodical name”,
+              “L1” : “File Attachments”,
+              “LK” : “Links”,
+              “N1” : “Notes”,
+              “N2” : “Abstract”,
+              “PB : “Publisher”,
+              “RD” : “Retrieved Date”,
+              “RN” : “Research Notes”,
+              “SN” : “ISSN, ISBN, or report/document/patent number”,
+              “SR” : “Source type- Print  0  or Electronic 1 ”,
+              “T1” : “(Primary) Title”,
+              “T2” : “Secondary title”,
+              “TA” : “Tertiary title”,
+              “TT” : “Translated title”,
+              “TY” : “Type of reference - must be the first tag”,
+              “UR” : “web/URL, can be repeated for multiple tags, or multiple URLs can be entered in a semicolon separated list”,
+              “WT” : “Website title”,
+              “WV” : “Website version”,
+              “Y1” : “Year///Date  Primary Date/year”,
+              “YR” : “Publication year”
+            }
+
+        }
+
+        
+    }
+
+
+
